1939b6a682fe36f9317774bada940b9704664898
Casserole Bot Pi <casserole_bot_pi@not_a_real_email.net>
2020-07-23 18:17:38 -0500

    Per https://stackoverflow.com/questions/63027848/discord-py-glitch-or-random-error-typeerror-new-got-an-unexpected-keywor, patched our old version of discord.py to account for a change in their api. Here's to hoping this doesn't happen often

--- casseroleBot.py
+++ casseroleBot.py
@@ -262,11 +262,13 @@ class CasseroleDiscordBotClient(discord.Client):
 
                     else:
                         #Unrecognized message. Flail about using chief delphi to generate a maybe reasonable response.
-                        response = "Ummmm... " + self.markovModel.make_short_sentence(300)
+                        response = self.markovModel.make_short_sentence(300)
 
                         if(response is None):
                             #Even chief delphi has failed us. Give up totally.
                             response = "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."
+                        else:
+                            response = "Ummmm... " + response
                         
             await message.channel.send(response)
 
--- discord/abc.py
+++ discord/abc.py
@@ -159,7 +159,23 @@ class PrivateChannel(metaclass=abc.ABCMeta):
             return NotImplemented
         return NotImplemented
 
-_Overwrites = namedtuple('_Overwrites', 'id allow deny type')
+class _Overwrites:
+    __slots__ = ('id', 'allow', 'deny', 'type')
+
+    def __init__(self, **kwargs):
+        self.id = kwargs.pop('id')
+        self.allow = kwargs.pop('allow', 0)
+        self.deny = kwargs.pop('deny', 0)
+        self.type = kwargs.pop('type')
+
+    def _asdict(self):
+        return {
+            'id': self.id,
+            'allow': self.allow,
+            'deny': self.deny,
+            'type': self.type,
+        }
+
 
 class GuildChannel:
     """An ABC that details the common operations on a Discord guild channel.
